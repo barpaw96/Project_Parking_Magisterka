4fbcf862fe9c6454dba3299c2710a178b3063de8
nsai-12 <239567@edu.p.lodz.pl>
2023-09-02 21:18:17 +0200

    doddanie sprawdzenia poprawności tablicy rejestracyjnej

--- main.py
+++ main.py
@@ -38,23 +38,27 @@ while True:
         thresh = cv2.threshold(foto2, 110, 255, cv2.THRESH_BINARY)[1]
         from pytesseract import image_to_string
         odczyt_tablicy_activ = image_to_string(thresh, lang='eng', config='--psm 9')
-        tablica_edyt_active = re.sub("[^A-Z0-9]", "", tablica_edyt_active)
-        sql_tablice_active = "SELECT id, name, idcolor_car, number_plates FROM users WHERE number_plates = %s"
 
-        cursor = connection.cursor()
-        cursor.execute(sql_tablice_active, (tablica_edyt_active,))
-        bramka_active = 0
+        if odczyt_tablicy_activ[0] or odczyt_tablicy_activ[3] == 32:
+            tablica_edyt_active = re.sub("[^A-Z0-9]", "", tablica_edyt_active)
+            sql_tablice_active = "SELECT id, name, idcolor_car, number_plates FROM users WHERE number_plates = %s"
 
-        for row in cursor:
-            print(row)
-            bramka_active = 1
+            cursor = connection.cursor()
+            cursor.execute(sql_tablice_active, (tablica_edyt_active,))
+            bramka_active = 0
 
-        if bramka_active == 0:
-            print("BRAMA ZAMKNIĘTA")
-        elif bramka_active == 1:
-            print("BRAMA OTWARTA")
+            for row in cursor:
+                print(row)
+                bramka_active = 1
+                print(row[1])
+            if bramka_active == 0:
+                print("BRAMA ZAMKNIĘTA")
+            elif bramka_active == 1:
+                print("BRAMA OTWARTA")
+                break
+        else:
+            print("Tablica jest niepoprawna")
             break
-
 
 
     cv2.imshow('img',img)
@@ -82,8 +86,14 @@ while True:
 
     from pytesseract import image_to_string
     odczyt_tabicy = image_to_string(thresh, lang='eng', config='--psm 9')
+
+    odczyt_tabicy = odczyt_tabicy.lstrip()
+    print(len(odczyt_tabicy))
+    print(odczyt_tabicy[7])
     tablica_edyt_static = odczyt_tabicy.rstrip()
+
     print(odczyt_tabicy)
+
 
     if tablica_edyt_static[2] or tablica_edyt_static[3] == 32:
         tablica_edyt_static = re.sub("[^A-Z0-9]", "", tablica_edyt_static)
@@ -97,14 +107,13 @@ while True:
         for row in cursor:
             print(row)
             bramka_static = 1
-
+            print(row[1])
         if bramka_static == 0:
             print("BRAMA ZAMKNIĘTA")
         elif bramka_static == 1:
             print("BRAMA OTWARTA")
             break
-    else:
-        print("Tablica nie jest poprawnie rozpoznana")
+
     #print(len(tablica_edyt_static))
 
 
@@ -115,8 +124,8 @@ while True:
     #plt.imshow(foto2)
     #plt.show()
 
-    plt.imshow(thresh)
-    plt.show()
+        plt.imshow(thresh)
+        plt.show()
 
     #print(height)
     #print(yheight)
@@ -124,132 +133,147 @@ while True:
     #print(xwith)
 
     #test_obraz1 = cv2.rectangle(foto, (width+120,height-480), (xwith+80, yheight-520), (255,255,0), 5)
-    cut_color = foto[height:yheight, width:xwith, :]
+        cut_color = foto[height:yheight, width:xwith, :]
     #plt.imshow(test_obraz1)
     #plt.show()
-    plt.imshow(cut_color)
-    plt.show()
-    cut_x = (xwith - width) / 2
-    cut_y = (yheight - height) / 2 # trzeba zamienić na liczbę całkowitą
+        plt.imshow(cut_color)
+        plt.show()
+        cut_x = (xwith - width) / 2
+        cut_y = (yheight - height) / 2 # trzeba zamienić na liczbę całkowitą
 
-    print(cut_y)
-    print(cut_x)
-    px = cut_color[90,240]
-    print(px)
+        print(cut_y)
+        print(cut_x)
+        px = cut_color[90,240]
+        print(px)
 
-    if px[0] >= 250 and px[1] <= 5 and px[2] <= 5:
-        print('czerwony')
-        read_color = "Czerwony"
-    elif px[0] <= 5 and px[1] >= 250 and px[2] <= 5:
-        print('zielony')
-        read_color = "Zielony"
-    elif px[0] <= 5 and px[1] <= 5 and px[2] >= 250:
-        print('niebieski')
-        read_color = "Niebieski"
-    elif px[0] >= 250 and px[1] >= 250 and px[2] <= 5:
-        print('żółty')
-        read_color = "Żółty"
-    elif px[0] >= 240 and px[1] <= 5 and px[2] >= 240:
-        print('fioletowy')
-        read_color = "Fioletowy"
-    elif px[0] <= 5 and px[1] >= 240 and px[2] >= 240:
-        print('turkusowy')
-        read_color = "Turkusowy"
-    elif px[0] >= 250 and px[1] >= 250 and px[2] >= 250:
-        print('biały')
-        read_color = "Biały"
-    elif px[0] <= 5 and px[1] <= 5 and px[2] <= 5:
-        print('czarny')
-        read_color = "Czarny"
-    elif px[0] >= 128 and px[1] <= 20 and px[2] <= 20:
-        print('bordo')
-        read_color = "Bordo"
-    elif px[0] >= 110 and px[1] >= 120 and px[2] >= 120:
-        print('szary')
-        read_color = "Szary"
-    elif px[0] <= 10 and px[1] >= 42 and px[2] >= 110:
-        print('granatowy')
-        read_color = "Granatowy"
-    elif px[0] >= 240 and px[1] >= 115 and px[2] <= 5:
-        print('pomarańczowy')
-        read_color = "Pomarańcz"
+        if px[0] >= 250 and px[1] <= 5 and px[2] <= 5:
+            print('czerwony')
+            read_color = "Czerwony"
+        elif px[0] <= 5 and px[1] >= 250 and px[2] <= 5:
+            print('zielony')
+            read_color = "Zielony"
+        elif px[0] <= 5 and px[1] <= 5 and px[2] >= 250:
+            print('niebieski')
+            read_color = "Niebieski"
+        elif px[0] >= 250 and px[1] >= 250 and px[2] <= 5:
+            print('żółty')
+            read_color = "Żółty"
+        elif px[0] >= 240 and px[1] <= 5 and px[2] >= 240:
+            print('fioletowy')
+            read_color = "Fioletowy"
+        elif px[0] <= 5 and px[1] >= 240 and px[2] >= 240:
+            print('turkusowy')
+            read_color = "Turkusowy"
+        elif px[0] >= 250 and px[1] >= 250 and px[2] >= 250:
+            print('biały')
+            read_color = "Biały"
+        elif px[0] <= 5 and px[1] <= 5 and px[2] <= 5:
+            print('czarny')
+            read_color = "Czarny"
+        elif px[0] >= 128 and px[1] <= 20 and px[2] <= 20:
+            print('bordo')
+            read_color = "Bordo"
+        elif px[0] >= 110 and px[1] >= 120 and px[2] >= 120:
+            print('szary')
+            read_color = "Szary"
+        elif px[0] <= 10 and px[1] >= 42 and px[2] >= 110:
+            print('granatowy')
+            read_color = "Granatowy"
+        elif px[0] >= 240 and px[1] >= 115 and px[2] <= 5:
+            print('pomarańczowy')
+            read_color = "Pomarańcz"
 
 
-    VW = "pusty"
-    HY = "pusty"
-    REN = "pusty"
-    AD = "pusty"
-    TY = "pusty"
+        VW = "pusty"
+        HY = "pusty"
+        REN = "pusty"
+        AD = "pusty"
+        TY = "pusty"
 
-    vw = vw_cascade.detectMultiScale(foto) ## działa poprawnie może trzeba zmienić model
-    toyota = toyota_cascade.detectMultiScale(foto)
-    audi = audi_cascade.detectMultiScale(foto) ## to samo co vw
-    renault = renault_casade.detectMultiScale(foto) ## działa super
-    hyundai = hyundai_casade.detectMultiScale(foto)
+        vw = vw_cascade.detectMultiScale(foto) ## działa poprawnie może trzeba zmienić model
+        toyota = toyota_cascade.detectMultiScale(foto)
+        audi = audi_cascade.detectMultiScale(foto) ## to samo co vw
+        renault = renault_casade.detectMultiScale(foto) ## działa super
+        hyundai = hyundai_casade.detectMultiScale(foto)
 
-    for (x, y, w, h) in vw:
-        VW = "Volkswagen"
-        foto3 = cv2.rectangle(foto, (x,y), (x+w, y+h), (255,255,0), 2)
+        for (x, y, w, h) in vw:
+            VW = "Volkswagen"
+            foto3 = cv2.rectangle(foto, (x,y), (x+w, y+h), (255,255,0), 2)
 
-        # cv2.rectangle(img, (x,y), (xw, y+h), (255,255,0), 5)
-    for (x, y, w, h) in renault:
-        REN = "Renault"
-        foto3 = cv2.rectangle(foto, (x,y), (x+w, y+h), (0,255,0), 5)
+            # cv2.rectangle(img, (x,y), (xw, y+h), (255,255,0), 5)
+        for (x, y, w, h) in renault:
+            REN = "Renault"
+            foto3 = cv2.rectangle(foto, (x,y), (x+w, y+h), (0,255,0), 5)
 
-    for (x, y, w, h) in toyota:
-        TY = "Toyota"
-        foto3 = cv2.rectangle(foto, (x,y), (x+w, y+h), (125,125,125), 10)
+        for (x, y, w, h) in toyota:
+            TY = "Toyota"
+            foto3 = cv2.rectangle(foto, (x,y), (x+w, y+h), (125,125,125), 10)
 
-    for (x, y, w, h) in audi:
-        AD = "Audi"
-        foto3 = cv2.rectangle(foto, (x,y), (x+w, y+h), (255,0,0), 15)
+        for (x, y, w, h) in audi:
+            AD = "Audi"
+            foto3 = cv2.rectangle(foto, (x,y), (x+w, y+h), (255,0,0), 15)
 
-    for (x, y, w, h) in hyundai:
-        HY = "Hyundai"
-        foto3 = cv2.rectangle(foto, (x,y), (x+w, y+h), (0,0,0), 20)
+        for (x, y, w, h) in hyundai:
+            HY = "Hyundai"
+            foto3 = cv2.rectangle(foto, (x,y), (x+w, y+h), (0,0,0), 20)
 
-    plt.imshow(foto3)
-    plt.show()
+        plt.imshow(foto3)
+        plt.show()
 
-    print(VW)
-    print(AD)
-    print(HY)
-    print(TY)
-    print(REN)
+        print(VW)
+        print(AD)
+        print(HY)
+        print(TY)
+        print(REN)
+        lenrowHY = 0
+        lenrowTY = 0
+        lenrowREN = 0
+        lenrowVW = 0
+        lenrowAD = 0
+        if VW == "Volkswagen":
+            sql_VW = "SELECT row_number() over (order by id), name, idcolor_car, idvehicle_brand, number_plates FROM users WHERE idvehicle_brand = %s AND idcolor_car =%s"
+            cursor = connection.cursor()
+            cursor.execute(sql_VW, (VW,read_color,))
+            for rowVW in cursor:
+                print(rowVW)
+                lenrowVW = rowVW[0]
 
-    if VW == "Volkswagen":
-        sql_VW = "SELECT id, name, idcolor_car, idvehicle_brand, number_plates FROM users WHERE idvehicle_brand = %s AND idcolor_car =%s"
-        cursor = connection.cursor()
-        cursor.execute(sql_VW, (VW,read_color,))
-        for row in cursor:
-            print(row)
-    if AD == "Audi":
-        sql_AD = "SELECT id, name, idcolor_car, idvehicle_brand, number_plates FROM users WHERE idvehicle_brand = %s AND idcolor_car =%s"
-        cursor = connection.cursor()
-        cursor.execute(sql_AD, (AD,read_color,))
-        for row in cursor:
-            print(row)
-    if TY == "Toyota":
-        sql_TY = "SELECT id, name, idcolor_car, idvehicle_brand, number_plates FROM users WHERE idvehicle_brand = %s AND idcolor_car =%s"
-        cursor = connection.cursor()
-        cursor.execute(sql_TY, (TY,read_color))
-        for row in cursor:
-            print(row)
-    if HY == "Hyundai":
-        sql_TY = "SELECT id, name, idcolor_car, idvehicle_brand, number_plates FROM users WHERE idvehicle_brand = %s AND idcolor_car =%s"
-        cursor = connection.cursor()
-        cursor.execute(sql_TY, (TY,read_color))
-        for row in cursor:
-            print(row)
-    if REN == "Renault":
-        sql_REN = "SELECT id, name, idcolor_car, idvehicle_brand, number_plates FROM users WHERE idvehicle_brand = %s AND idcolor_car =%s"
-        cursor = connection.cursor()
-        cursor.execute(sql_REN, (REN,read_color))
-        for row in cursor:
-            print(row)
+        if AD == "Audi":
+            sql_AD = "SELECT row_number() over (order by id), name, idcolor_car, idvehicle_brand, number_plates FROM users WHERE idvehicle_brand = %s AND idcolor_car =%s"
+            cursor = connection.cursor()
+            cursor.execute(sql_AD, (AD,read_color,))
+            for rowAD in cursor:
+                print(rowAD)
+                lenrowAD = rowAD[0]
+        if TY == "Toyota":
+            sql_TY = "SELECT row_number() over (order by id), name, idcolor_car, idvehicle_brand, number_plates FROM users WHERE idvehicle_brand = %s AND idcolor_car =%s"
+            cursor = connection.cursor()
+            cursor.execute(sql_TY, (TY,read_color))
+            for rowTY in cursor:
+                print(rowTY)
+                lenrowTY = rowTY[0]
+        if HY == "Hyundai":
+            sql_TY = "SELECT row_number() over (order by id), name, idcolor_car, idvehicle_brand, number_plates FROM users WHERE idvehicle_brand = %s AND idcolor_car =%s"
+            cursor = connection.cursor()
+            cursor.execute(sql_TY, (TY,read_color))
+            for rowHY in cursor:
+                print(rowHY)
+                lenrowHY = rowHY[0]
+        if REN == "Renault":
+            sql_REN = "SELECT row_number() over (order by id), name, idcolor_car, idvehicle_brand, number_plates FROM users WHERE idvehicle_brand = %s AND idcolor_car =%s"
+            cursor = connection.cursor()
+            cursor.execute(sql_REN, (REN,read_color))
+            for rowREN in cursor:
+                print(rowREN)
+                lenrowREN = rowREN[0]
+        lenend = lenrowREN + lenrowHY + lenrowTY + lenrowAD + lenrowVW
 
-    break
+        print(lenend)
 
+        break
+    else:
+        print(row)
+        print("Tablica nie jest poprawnie rozpoznana")
 
 
 
